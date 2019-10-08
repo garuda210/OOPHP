b4e9d164c73beda3c2c56b08b42780c88d13b59e
garuda210 <173040044@mail.unpas.ac.id>
2019-10-08 16:42:09 +0700

    inheritance

--- mobil.php
+++ mobil.php
@@ -1,13 +1,13 @@
 <?php
 class Mobil {
     public $nama,
-    public $merk,
-    public $warna,
-    public $kecepatanMaksimal,
-    public $jumlahPenumpang;
+     $merk,
+     $warna,
+     $kecepatanMaksimal,
+     $jumlahPenumpang;
 
     public function tambahKecepatan(){
-
+        return "kecepatan bertambah!";
     }
 
     public function kuranginKecepatan(){
@@ -15,6 +15,20 @@ class Mobil {
     }
 
     public function gantiTransmisi(){
-        
+
     }
 }
+
+class MobilSport extends Mobil{
+    public $turbo = false ;
+
+    public function jalankanTurbo(){
+        $this -> turbo = true;
+        return " Turbo dijalankan! ";
+    }
+}
+
+$mobil1 = new MobilSport();
+echo $mobil1-> tambahKecepatan();
+echo "<br>";
+echo $mobil1 -> jalankanTurbo();
